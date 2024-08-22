aa3abf44e8bc3bc9ccc3d84ad92c538da8355618
Tuba <brunogabrielsd@hotmail.com>
2024-08-21 22:48:41 -0300

    chore: contato route files

---
+++ src/pages/Contato/Contato.jsx
@@ -0,0 +1,12 @@
+import './Contato.css'
+
+function Contato() {
+  return (
+    <div className="contato">
+      <h2>Contato</h2>
+      <p>Esta é a página de Contato.</p>
+    </div>
+  )
+}
+
+export default Contato
