0fa393cdc668fd87b295446df81edb630b8c43fa
Tuba <brunogabrielsd@hotmail.com>
2024-08-21 22:45:45 -0300

    feat: add full width prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import './Button.css'
 
-function Button({ children, onClick, variant = 'default', size = 'medium' }) {
+function Button({ children, onClick, variant = 'default', size = 'medium', fullWidth = false }) {
   const classNames = `button button--${variant} button--${size}`;
 
   return (
-    <button className={classNames} onClick={onClick}>
+    <button className={classNames} onClick={onClick} style={{ width: fullWidth ? '100%' : 'auto' }}>
       {children}
     </button>
   )
